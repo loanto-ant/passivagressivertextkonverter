24f9421c1673607521b07e0c1635f2d471b62458
loanto-ant <lorenz.filla@gmail.com>
2022-10-13 21:45:47 +0200

    change

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 function convert(){
     let output=document.getElementById("output");
     let input=document.getElementById("input").value
+    output.innerText="";
     let txt=input;
     txt=txt.toLowerCase();
-    output.value=txt;
+
+    let buchstaben=txt.split("");
+    
+    buchstaben.forEach((element,index) => {
+        let random=Math.random();
+        if(random>0.5){
+            buchstaben[index]=element.toUpperCase()
+        }
+        output.innerText+=buchstaben[index];
+    });
 }
